322330ba200b3ff7ab413f90b212e915a09b5e78
achuth10 <43288837+achuth10@users.noreply.github.com>
2020-09-16 01:17:11 +0530

    Fixes

--- js/googleChart.js
+++ js/googleChart.js
@@ -13,7 +13,7 @@
 	]);
 	
 	  // Optional; add a title and set the width and height of the chart
-	  var options = {'title':'Total visitors', 'width':300, 'height':100};
+	  var options = {'title':'Total visitors', 'width':500, 'height':300};
 	
 	  // Display the chart inside the <div> element with id="piechart"
 	  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
@@ -32,7 +32,7 @@
 			var options = {
 			  title: 'Performance',
 			  curveType: 'function',
-			  'width':450, 'height':300,
+			  'width':600, 'height':300,
 			  legend: { position: 'bottom' }
 			};
 	
