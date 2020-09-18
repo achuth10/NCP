2c443bd3ef7cf6ea43040043b235c216c29b6045
achuth10 <43288837+achuth10@users.noreply.github.com>
2020-09-18 19:12:34 +0530

    Fixes

--- js/admin_panel.js
+++ js/admin_panel.js
@@ -2,5 +2,5 @@ document.getElementById("articleBtn").addEventListener("click",function(){
 	location.href="write_article.html"
 });
 document.getElementById("eventBtn").addEventListener("click",function(){
-	location.href="create_event.html"
+	location.href="event-register.html"
 });
