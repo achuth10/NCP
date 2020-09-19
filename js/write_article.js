48ba878e57c1ee3be6c4a7e202d5a0a1681cc78e
achuth10 <43288837+achuth10@users.noreply.github.com>
2020-09-19 12:13:35 +0530

    Added validation

--- js/write_article.js
+++ js/write_article.js
@@ -1,3 +1,15 @@
 function format(command) {
 	document.execCommand(command, false);
 }
+
+document.getElementById("publishBtn").addEventListener("click",function(){
+var headline = document.getElementById("fname").value;
+if (!headline){
+	alert("Please enter a headline");
+}
+
+var body = document.getElementById("editor").textContent;
+if (!body){
+	alert("Please enter the body of the article");
+}
+});
